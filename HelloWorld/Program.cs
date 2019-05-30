5baa9bb94e9700736bb84579fa58d5a9fad5161c
Richard-D-Taylor <richard.dewayne.taylor@gmail.com>
2019-05-29 19:19:57 -0500

    First C# program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    { // a field class member
+
+        const int number = 0;
+        const float one = 3.4f;
+
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+    }
+}
